baaa3c67ba3425abbe4f0b064e9d36859a8642a0
Jakub <gamernice223@gmail.com>
2021-10-30 22:39:45 +0200

    Add getAnswersFile function

---
+++ src/functions/getAnswersFile.ts
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+export const getAnswersFile = async (id: string): Promise<boolean> => {
+  try {
+    const answersBlob = await axios.post(
+      `${process.env.API_URL}/getAnswers`,
+      {
+        id,
+      },
+      { responseType: "blob" }
+    );
+    const fileName = `quiz_${id}_answers.pdf`;
+    const file = new Blob([answersBlob.data], { type: "application/pdf" });
+
+    await saveAs(file, fileName);
+    return true;
+  } catch {
+    return false;
+  }
+};
