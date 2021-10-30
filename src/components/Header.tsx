99b2d683545b71317f7cb8e257babc61edb5006d
Jakub <gamernice223@gmail.com>
2021-10-30 20:41:33 +0200

    Add FlexContainer component

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+
+const Title = styled.h1`
+  color: #c14953;
+  font-size: 5rem;
+`;
+
+export const Header = () => (
+  <header>
+    <Title>Quizizz Parser</Title>
+  </header>
+);
