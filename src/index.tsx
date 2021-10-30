6a97c7bf721cf249a1e30947d0960bed2ffd2f6f
Jakub <gamernice223@gmail.com>
2021-10-30 20:40:35 +0200

    Add global style

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom";
 import reportWebVitals from "./reportWebVitals";
 import { Main } from "./Main";
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+    body {
+        margin: 0;
+        padding: 0;
+        background: #2D2D2A;
+        font-family: 'Roboto', sans-serif;
+    }
+`;
 
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyle />
     <Main />
   </React.StrictMode>,
   document.getElementById("root")
